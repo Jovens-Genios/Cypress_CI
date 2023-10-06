8ae89cd5cfccb6b077df7ec4e3283c525de0b409
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-06 12:51:10 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,25 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.google.com')
-  })
+  
+  //.........................................................................................................................
+    it('CT001 - Se a escola estiver dentro do período de Olimpíadas, deverá aparecer a mensagem de aviso', () => {
+
+        cy.visit(mt.Url)
+    
+        //Login
+        cy.get("[data-cy=email]").type(mt.Usuario)
+        cy.get("[data-cy=password]").type(mt.Senha)
+        cy.get(".v-btn__content > .row > .d-flex").click()
+    
+        //Nova Atividade
+        cy.get("[data-cy=home-create] > .row > .iconHolder").click()
+    
+        //Nova Tarefa
+        cy.get("[data-cy=TAREFA] > .rounded-lg").click();
+    
+        //fechar Mensagem
+        cy.get(".pt-9 > :nth-child(2) > .v-btn").click();
+    
+        cy.get('zzz').should("be.visible");
+    });
+  
 })
