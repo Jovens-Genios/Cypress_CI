b7929a8a138b988e58428bcbab6ab25976b89ebd
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-08 00:46:07 -0300

    Update Testes_Regressivos_Nova_Tarefa.cy.js

--- cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
+++ cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
@@ -15,11 +15,9 @@ describe('template spec', () => {
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[3]/div/div/div[1]/div/input').type("educadortesteqa@jovensgenios.com")
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[4]/div/div/div[1]/div[1]/input').type("jovensgenios")
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[7]/button/span/div/div/span').click()
-    cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/div/div/div[1]/div[2]/div[1]/div/div/span').click()
-    cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/section/div/div[2]/div/div/div[3]/div/img').click()
-    cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/section/div[1]/button/span/span').click()
+    cy.visit('https://dev-educadores.jovensgenios.com/atividades/tarefa/criar?step=1')
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/section/div[1]/section[1]/div/div[2]/div/div[1]/div/div/div/input').type('zzzz')
-
+    
     //cy.get(po.ID_Senha).type(mt.Senha)
     //cy.get(po.ID_Botao).click()
 
