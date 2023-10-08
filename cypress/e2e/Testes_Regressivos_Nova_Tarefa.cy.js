b4a8c2421f5db31bed9da1c73db2eb8cda27c4bc
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-07 23:30:08 -0300

    Update Testes_Regressivos_Nova_Tarefa.cy.js

--- cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
+++ cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
@@ -6,12 +6,10 @@ describe('template spec', () => {
   
   it('CT013 - Ao clicar em “Editar” o usuário deverá ser direcionado à etapa referida e os dados devem estar preenchidos com o que está sendo apresentado no Resumo.', () => {
 
-   
-cy.visit('https://www.google.com.br/');
-cy.get('form > div:nth-child(1)').click();
-cy.get('#APjFqb').click();
-cy.url().should('contains', 'https://www.google.com.br/search');
-cy.url().should('contains', 'https://ogs.google.com.br/u/0/widget/app');
+    
+    cy.visit("http://www.google.com")
+    cy.get('#APjFqb').type("oooo");
+    
    
 });
 
