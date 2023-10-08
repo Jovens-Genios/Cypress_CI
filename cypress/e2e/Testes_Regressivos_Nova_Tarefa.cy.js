f152120488a0d4748fc00e9200f206c8a74c280b
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-08 02:00:33 -0300

    Add files via upload

--- cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
+++ cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
@@ -8,19 +8,24 @@ describe('template spec', () => {
 
   it('CT013 - Ao clicar em “Editar” o usuário deverá ser direcionado à etapa referida e os dados devem estar preenchidos com o que está sendo apresentado no Resumo.', () => {
 
-   
+
     cy.visit(mt.Url)
 
     //Login
-    cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[3]/div/div/div[1]/div/input').type("educadortesteqa@jovensgenios.com")
-    cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[4]/div/div/div[1]/div[1]/input').type("jovensgenios")
-    cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[7]/button/span/div/div/span').click()
-        cy.wait(5000)
-
-       cy.get('iframe[id="Your project:Test Project"]')
-        cy.xpath('/html/body/div[8]/div[2]/div[1]/main/div/main/div/div/div[1]/div[2]/div[1]/div').click()
-
+    cy.xpath('//*[@id="input-15"]').type("educadortesteqa@jovensgenios.com")
+    cy.xpath('//*[@id="input-18"]').type("jovensgenios")
+    cy.xpath('//*[@id="base"]/div/div[1]/main/div/div[1]/div/div/div/div[7]/button/span/div/div/span').click()
     
+    cy.get('Iframe')
+
+    cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/div/div/div[1]/div[2]/div[1]/div/div/span').click()
+    
+    //cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/section/div/div[2]/div/div/div[3]/div/img').click()
+
+    //cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/section/div[1]/button/span/span').click()
+
+    //cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/section/div[1]/section[1]/div/div[2]/div/div[1]/div/div/div/input').type('zzzz')
+
     //cy.get(po.ID_Senha).type(mt.Senha)
     //cy.get(po.ID_Botao).click()
 
