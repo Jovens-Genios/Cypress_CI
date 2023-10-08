5f821602c726e027eb6777c9424e15f73649caa7
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-07 23:08:09 -0300

    Update Testes_Regressivos_Nova_Tarefa.cy.js

--- cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
+++ cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
@@ -8,27 +8,12 @@ describe('template spec', () => {
   
   it('CT013 - Ao clicar em “Editar” o usuário deverá ser direcionado à etapa referida e os dados devem estar preenchidos com o que está sendo apresentado no Resumo.', () => {
 
-    cy.visit(mt.Url)
+cy.visit('https://dev-educadores.jovensgenios.com/atividades/tarefa/criar?step=1');
+cy.get('[data-cy=input-activity-name]').click();
+cy.get('[data-cy=input-activity-name]').type('xxxx');
+cy.get('.v-btn__content > .white--text').click();
+cy.get('[data-cy=school]').click();
 
-    //Login
-    cy.get(po.ID_Usuario).type(mt.Usuario);
-    cy.get(po.ID_Senha).type(mt.Senha);
-    cy.get(po.ID_Botao).click();
-    cy.wait(5000);
-    
-    //Nova Atividade
-    cy.visit("https://dev-educadores.jovensgenios.com/criar");
-    cy.wait(5000);
-
-    //Nova Tarefa
-    cy.visit("https://dev-educadores.jovensgenios.com/atividades/ASSIGNMENT");
-    cy.wait(5000);
-
-    //fechar Mensagem
-    cy.visit("https://dev-educadores.jovensgenios.com/atividades/tarefa/criar?step=1",{timeout:10000});
-
-    //Nome da atividade
-    cy.get("[data-cy='input-activity-name']").type("xxx");
 
 });
 
