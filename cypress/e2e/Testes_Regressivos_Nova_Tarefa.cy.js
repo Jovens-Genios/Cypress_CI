22492d2a8a49a095a6661ab90c4c6794605efc37
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-08 01:43:39 -0300

    Update Testes_Regressivos_Nova_Tarefa.cy.js

--- cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
+++ cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
@@ -17,6 +17,7 @@ describe('template spec', () => {
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[7]/button/span/div/div/span').click()
         cy.wait(5000)
 
+       cy.get('iframe[id="Your project:Test Project"]')
         cy.xpath('/html/body/div[8]/div[2]/div[1]/main/div/main/div/div/div[1]/div[2]/div[1]/div').click()
 
     
