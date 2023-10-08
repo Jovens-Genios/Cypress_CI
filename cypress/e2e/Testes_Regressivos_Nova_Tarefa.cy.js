6c11793e333f57256685ce5d921a38fdef1d6044
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-08 00:56:10 -0300

    Update Testes_Regressivos_Nova_Tarefa.cy.js

--- cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
+++ cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
@@ -15,7 +15,7 @@ describe('template spec', () => {
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[3]/div/div/div[1]/div/input').type("educadortesteqa@jovensgenios.com")
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[4]/div/div/div[1]/div[1]/input').type("jovensgenios")
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[7]/button/span/div/div/span').click()
-    
+    cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/div/div/div[1]/div[2]/div[1]/div/div/span').click()
     
     //cy.get(po.ID_Senha).type(mt.Senha)
     //cy.get(po.ID_Botao).click()
