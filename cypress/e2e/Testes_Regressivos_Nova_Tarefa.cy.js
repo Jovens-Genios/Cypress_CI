18f43a0b16e2d5ce96d8f7b903a6d5efebbcddb6
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-09 14:14:39 -0300

    Update Testes_Regressivos_Nova_Tarefa.cy.js

--- cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
+++ cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
@@ -9,16 +9,13 @@ describe('template spec', () => {
   it('CT013 - Ao clicar em “Editar” o usuário deverá ser direcionado à etapa referida e os dados devem estar preenchidos com o que está sendo apresentado no Resumo.', () => {
 
 
-    cy.visit(mt.Url)
+   cy.visit(mt.Url)
 
     //Login
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[3]/div/div/div[1]/div/input').type("educadortesteqa@jovensgenios.com")
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[4]/div/div/div[1]/div[1]/input').type("jovensgenios")
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[7]/button/span/div/div/span').click()
-    
-
-    cy.iframe('[id="Your project: 'Test Project'"]').find('[data-cy="home-create"] > .row > .fs-18').click()
-    cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/div/div/div[1]/div[2]/div[1]/div/div/span').click()
+    cy.get('[data-cy="home-create"] > .row > .fs-18').click()
     
     //cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/section/div/div[2]/div/div/div[3]/div/img').click()
 
