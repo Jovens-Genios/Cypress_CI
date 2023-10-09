194a46a202e740d745ed63b9fae8ed948efb7d85
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2023-10-09 14:46:38 -0300

    Update Testes_Regressivos_Nova_Tarefa.cy.js

--- cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
+++ cypress/e2e/Testes_Regressivos_Nova_Tarefa.cy.js
@@ -15,28 +15,6 @@ describe('template spec', () => {
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[3]/div/div/div[1]/div/input').type("educadortesteqa@jovensgenios.com")
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[4]/div/div/div[1]/div[1]/input').type("jovensgenios")
     cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/div/div/div/div[7]/button/span/div/div/span').click()
-    cy.visit('https://dev-educadores.jovensgenios.com/login')
-    cy.get('[data-cy="home-create"] > .row > .fs-18').click()
-
-
-    
-    //cy.xpath('/html/body/div[8]/div/div[1]/main/div/div[1]/section/div/div[2]/div/div/div[3]/div/img').click()
-
-    //cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/section/div[1]/button/span/span').click()
-
-    //cy.xpath('/html/body/div[8]/div/div[1]/main/div/main/section/div[1]/section[1]/div/div[2]/div/div[1]/div/div/div/input').type('zzzz')
-
-    //cy.get(po.ID_Senha).type(mt.Senha)
-    //cy.get(po.ID_Botao).click()
-
-     //Nova Atividade
-    //cy.get(po.ID_Nova_Atividade).click({ force: true })
-
-    //Nova Tarefa
-    //cy.get(po.ID_Nova_Tarefa).click({ force: true });
-
-    //fechar Mensagem
-    //cy.get(po.ID_Fechar_Mensagem).click({ force: true });
    
 });
 
